952d3b072073a828a75c2ae3bbf8b6ac44db89ce
Willians Vargas <williant.marquez@gmail.com>
2023-01-21 16:30:57 +0100

    [FIX] Removed automatic DB sync

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,6 @@ import { ConfigModule } from '@nestjs/config';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      synchronize: true,
       autoLoadEntities: true,
     }),
     CategoriesModule,
