b6c5da4084f5a88e77bfcf373a3df6a8b6cc2b85
Willians Vargas <williant.marquez@gmail.com>
2023-01-22 18:04:34 +0100

    [FEAT] Adding swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { getValidationPipe } from './getValidationPipe';
 
@@ -6,6 +7,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(getValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Cocktails API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
 
   await app.listen(process.env.PORT || 3000, '0.0.0.0', async () => {
     console.log(`Application is running on: ${await app.getUrl()}`);
